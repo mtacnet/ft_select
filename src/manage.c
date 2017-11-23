ca990a89ebf15c1ba46afd3a581cac96a58a9ab7
mtacnet <mtacnet@mtacnets-Mac.local>
2017-11-23 09:47:33 +0000

    Optimisation de la fonction 'manage' permettant la gestion des flags de la structure t_elem - Verification des leaks == OK

--- src/manage.c
+++ src/manage.c
@@ -12,21 +12,27 @@
 
 #include "../includes/ft_select.h"
 
+static void		manage_key(char *buff, t_elem *tmp)
+{
+	if (buff[0] == 32)
+	{
+		tmp->ul = 0;
+		tmp->hl = 1;
+		if (tmp->next != NULL)
+			tmp->next->ul = 1;
+	}
+}
+
 int		manage(char *buff, t_elem **e, int pos)
 {
 	t_elem		*tmp;
+
 	tmp = (*e);
 	while (tmp != NULL && tmp->nb < pos)
 		tmp = tmp->next;
 	if (tmp != NULL)
 	{
-		if (buff[0] == 32)
-		{
-			tmp->ul = 0;
-			tmp->hl = 1;
-			if (tmp->next != NULL)
-				tmp->next->ul = 1;
-		}
+		manage_key(buff, tmp);
 	}
 	if (tmp->next != NULL)
 		return (tmp->next->nb);
